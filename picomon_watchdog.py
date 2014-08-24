8a8842e1d7a32bbfa201f873f8eb75297f91d047
Jonathan Michalon <jonathan@michalon.eu>
2014-08-24 16:35:14 +0200

    Support commandline-passed config file in picomon_watchdog

--- picomon_watchdog.py
+++ picomon_watchdog.py
@@ -1,11 +1,29 @@
 import subprocess
-import config
+import sys
+import os
+import importlib
+from picomon import config
 from picomon import mails
 
 
-retcode = subprocess.call(["python3", "picomon.py"])
+# launch picomon
+retcode = subprocess.call(["python3", "picomon.py"] + sys.argv[1:])
 
+# load config file
+# (unprotected, will trigger exceptions if problems but real picomon beforehand)
+configfile = "config.py"
+
+if "-c" in sys.argv:
+  configfile = sys.argv[sys.argv.index("-c")+1]
+if "--config" in sys.argv:
+  configfile = sys.argv[sys.argv.index("--config")+1]
+
+sys.path.append(os.path.dirname(configfile))
+filename  = os.path.basename(configfile)
+base, ext = os.path.splitext(filename)
+importlib.import_module(base)
+
+# send warning
 mails.send_email(config.emails.watchdog_subject,
                  "Picomon exited with status %s" % retcode)
 
-mails.quit()
